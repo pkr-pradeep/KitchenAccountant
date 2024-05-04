b69769a82f427c5fd413f1d2de7cf17dbf066bf2
Pradeep Rout <22601317+pkr-pradeep@users.noreply.github.com>
2024-05-05 00:48:05 +0530

    initial commit

--- app/src/main/java/com/example/kitchenaccountant/ui/income/IncomeFragment.java
+++ app/src/main/java/com/example/kitchenaccountant/ui/income/IncomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.kitchenaccountant.ui.income;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,8 +16,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.kitchenaccountant.R;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Calendar;
 
 public class IncomeFragment extends Fragment {
 
@@ -28,6 +28,7 @@ public class IncomeFragment extends Fragment {
     private Button submitButton;
     private IncomeViewModel viewModel;
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_incomes, container, false);
@@ -35,9 +36,15 @@ public class IncomeFragment extends Fragment {
         categorySpinner = root.findViewById(R.id.spinner_category);
         collectedFromEditText = root.findViewById(R.id.edit_text_collected_from);
         amountEditText = root.findViewById(R.id.edit_text_amount);
-        receivedDateEditText = root.findViewById(R.id.edit_text_received_date);
         remarksEditText = root.findViewById(R.id.edit_text_remarks);
         submitButton = root.findViewById(R.id.button_submit_income);
+        receivedDateEditText = root.findViewById(R.id.edit_text_received_date);
+        receivedDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePickerDialog();
+            }
+        });
 
         /*// Populate income category dropdown
         List<String> categories = new ArrayList<>();
@@ -78,5 +85,21 @@ public class IncomeFragment extends Fragment {
         });
 
         return root;
+    }
+
+    private void showDatePickerDialog() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(requireContext(),
+                (datePicker, year1, month1, day) -> {
+                    // Update the received date EditText with the selected date
+                    String dateString = String.format("%02d/%02d/%d", day, month1 + 1, year1);
+                    receivedDateEditText.setText(dateString);
+                }, year, month, dayOfMonth);
+
+        datePickerDialog.show();
     }
 }
